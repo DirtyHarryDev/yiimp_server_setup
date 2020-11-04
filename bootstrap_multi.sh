5e6c18d79bbffb5a9a024c207bf31871f211d864
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-11-04 23:16:07 +0100

    Update bootstrap_multi.sh

--- bootstrap_multi.sh
+++ bootstrap_multi.sh
@@ -6,34 +6,14 @@
 # This script is intended to be ran from the Yiimp Server Installer            #
 ################################################################################
 
-if [ -z "${TAG}" ]; then
-	TAG=v1.0
-fi
-
 # Clone the MultiPool repository if it doesn't exist.
 if [ ! -d $HOME/yiimpserver/yiimp_multi ]; then
 	echo Downloading Dirty Harry Yiimp Multi Server Installer ${TAG}. . .
 	git clone \
-		-b ${TAG} --depth 1 \
 		https://github.com/DirtyHarryDev/server_yiimp_multi \
 		$HOME/yiimpserver/yiimp_multi \
 		< /dev/null 2> /dev/null
 
-	echo
-fi
-
-# Change directory to it.
-cd $HOME/yiimpserver/yiimp_multi
-
-# Update it.
-sudo chown -R $USER $HOME/yiimpserver/install/.git/
-if [ "${TAG}" != `git describe --tags` ]; then
-	echo Updating Dirty Harry Yiimp Multi Server Installer to ${TAG} . . .
-	git fetch --depth 1 --force --prune origin tag ${TAG}
-	if ! git checkout -q ${TAG}; then
-		echo "Update failed. Did you modify something in `pwd`?"
-		exit
-	fi
 	echo
 fi
 
