25a2dbc24c8b6f76fedc43c3a25c5aa5a5294ac9
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-11-08 17:43:27 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,13 +9,11 @@ source /etc/functions.sh
 RESULT=$(dialog --stdout --nocancel --default-item 1 --title "Dirty Harry Yiimp Server Installer v1.0" --menu "Choose one" -1 60 16 \
 ' ' "- YiiMP Server Install -" \
 1 "YiiMP Single Server" \
-2 "YiiMP Multi Server" \
-3 Exit)
+2 Exit)
 if [ $RESULT = ]
 then
 bash $(basename $0) && exit;
 fi
-
 
 if [ $RESULT = 1 ]
 then
@@ -25,13 +23,6 @@ source bootstrap_single.sh;
 fi
 
 if [ $RESULT = 2 ]
-then
-clear;
-cd $HOME/yiimpserver/install
-source bootstrap_multi.sh;
-fi
-
-if [ $RESULT = 3 ]
 then
 clear;
 exit;
